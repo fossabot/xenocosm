4b6171a67ba9bc197d2e233635922ae54a8c886b
Doug Hurst <dalan.hurst@gmail.com>
2017-04-30 20:51:47 -0600

    Added blurb to home screen

--- src/main/scala/xenocosm/app/service/HomeService.scala
+++ src/main/scala/xenocosm/app/service/HomeService.scala
@@ -20,20 +20,35 @@ object HomeService {
       |################################################################
       |################################################################""".stripMargin
 
+  private val blurb:String =
+    """
+      |
+      |Where have You hidden Yourself,
+      |And abandoned me in my groaning, O my Beloved?
+      |You have fled like the hart,
+      |Having wounded me.
+      |I ran after You, crying; but You were gone.
+      |
+      |St. John of the Cross, "A Spiritual Canticle", ca. 01578""".stripMargin
+
   val screen:fansi.Str =
     title.
       split("\n").
-      map(_.zipWithIndex.map({ case (c, i) ⇒ fansi.Color.True(i * 4, 255 - (i * 4), 255)(s"$c") }).mkString).
+      map(_.
+        zipWithIndex.
+        map({ case (c, i) ⇒ fansi.Color.True(i * 4, 255 - (i * 4), 255)(s"$c") }).
+        mkString).
       mkString("\n")
 
   val service = HttpService {
     case req @ GET -> Root ⇒
       getSeed(req) match {
         case Some(_) ⇒
-          Ok(screen).putHeaders(MultiverseService.location(req))
+          Ok(screen ++ fansi.Color.White(blurb)).
+            putHeaders(MultiverseService.location(req))
         case None ⇒
           val seed = Random.nextLong()
-          Ok(screen).
+          Ok(screen ++ fansi.Color.White(blurb)).
             addCookie(Cookie("seed", seed.toString)).
             putHeaders(MultiverseService.location(req))
       }
