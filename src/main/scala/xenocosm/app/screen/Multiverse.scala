f993708ee515ae59bf056ffb11c81941a3b83f37
Doug Hurst <dalan.hurst@gmail.com>
2017-05-13 12:22:47 -0600

    Fixed highlight on the Multiverse poem

--- src/main/scala/xenocosm/app/screen/Multiverse.scala
+++ src/main/scala/xenocosm/app/screen/Multiverse.scala
@@ -21,7 +21,23 @@ object Multiverse {
       |""".stripMargin
 
   // scalastyle:off magic.number
-  def apply:fansi.Str =
-    fansi.Color.True(0, 255, 255)(poem).overlay(fansi.Color.White, 227, 236)
+  def colorize(c:Char):fansi.Str = fansi.Color.True(0, 255, 255)(s"$c")
+  def colorize(str:String):fansi.Str = fansi.Color.True(0, 255, 255)(str)
   // scalastyle:on magic.number
+
+  private val highlight:String ⇒ fansi.Str = section ⇒
+    poem.split("\n").
+      map({
+        case line if line contains section ⇒
+          val start = line indexOf section
+          val end = start + section.length
+          line.zipWithIndex.map({
+            case (c, i) if i >= start && i < end ⇒ fansi.Color.White(s"$c")
+            case (c, _) ⇒ colorize(c)
+          }).mkString
+        case line => colorize(line)
+      }).
+      mkString("\n")
+
+  def apply:fansi.Str = highlight("colored like bone") ++ "\n"
 }
